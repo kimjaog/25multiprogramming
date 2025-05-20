9e20c7999965879e3c8d913a716c03e724264c5a
kimjaog <youngkim7250@gmail.com>
2025-05-20 14:52:55 +0900

    gg

--- tst/Project1/main.c
+++ tst/Project1/main.c
@@ -1,6 +1,20 @@
 #include<stdio.h>
 
+int SUM(int a, int b)
+{
+	return a + b;
+}
+
+int SUB(int a, int b)
+
+{
+	return a - b;
+}
+
 int main()
 {
-
+	int a = 10, b = 5;
+	printf("SUM of %d and %d is %d\n", a,b, SUM(a,b));
+	printf("SUB of %d and %d is %d\n", a, b, SUB(a, b));
+	return 0;
 }
